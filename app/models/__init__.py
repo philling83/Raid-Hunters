2bee0faf07729fd6a78ff0ff95df8f67d01e315d
philling83 <philipling83@gmail.com>
2021-04-13 14:12:04 -0400

    Create gym model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
 from .team import Team
+from .gym import Gym
---
+++ app/models/gym.py
@@ -0,0 +1,25 @@
+from .db import db
+
+
+class Gym(db.Model):
+    __tablename__ = "gyms"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.String(200), nullable=False, unique=True)
+    latitude = db.Column(db.Integer, nullable=False)
+    longitude = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    url_img = db.Column(db.String(200), unique=True)
+    
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "location": self.location,
+            "url_img": self.url_img,
+        }
