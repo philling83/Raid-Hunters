10e70f1077635cb6ceaa5c494e73111a518999b1
philling83 <philipling83@gmail.com>
2021-04-15 21:42:32 -0400

    Seed raids table

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .teams import seed_teams, undo_teams
 from .types import seed_types, undo_types
+from .raids import seed_raids, undo_raids
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,14 +11,16 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
-    seed_teams()
-    seed_users()
-    seed_types()
+    # seed_teams()
+    # seed_users()
+    # seed_types()
+    seed_raids()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_raids()
     undo_types()
     undo_teams()
     undo_users()
---
+++ app/seeds/raids.py
@@ -0,0 +1,25 @@
+from app.models import db, Raid
+
+
+def seed_raids():
+
+    raid1 = Raid(pokemon="Mankey", tier=1, url_img="https://play.pokemonshowdown.com/sprites/ani/mankey.gif",
+                 min_cp_range=616, max_cp_range=665, boosted_min_cp_range=770, boosted_max_cp_range=832)
+    raid2 = Raid(pokemon="Nidoking", tier=3, url_img="https://play.pokemonshowdown.com/sprites/ani/nidoking.gif",
+                 min_cp_range=1395, max_cp_range=1466, boosted_min_cp_range=1743, boosted_max_cp_range=1833)
+    raid3 = Raid(pokemon="Landorus (Therian Forme)", tier=5, url_img="https://play.pokemonshowdown.com/sprites/ani/landorus-therian.gif",
+                 min_cp_range=2151, max_cp_range=2241, boosted_min_cp_range=2688, boosted_max_cp_range=2801)
+    raid4 = Raid(pokemon="Mega Lopunny", tier=5, url_img="https://play.pokemonshowdown.com/sprites/ani/lopunny-mega.gif",
+                 min_cp_range=1112, max_cp_range=1177, boosted_min_cp_range=1391, boosted_max_cp_range=1471)
+
+    db.session.add(raid1)
+    db.session.add(raid2)
+    db.session.add(raid3)
+    db.session.add(raid4)
+
+    db.session.commit()
+
+
+def undo_raids():
+    db.session.execute('TRUNCATE raids RESTART IDENTITY CASCADE;')
+    db.session.commit()
