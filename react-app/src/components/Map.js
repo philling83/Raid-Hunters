21a655b314d89f8dc994fd4e4982e80cd9d48e42
philling83 <philipling83@gmail.com>
2021-08-05 12:49:11 -0400

    Test Material-UI Card component in Popup

--- react-app/src/components/Map.js
+++ react-app/src/components/Map.js
@@ -1,10 +1,36 @@
 // import React, { useEffect, useRef, useState } from 'react'
 // import L from 'leaflet'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import "./Map.css";
 
 function MapSimple() {
+
+    const useStyles = makeStyles({
+        root: {
+            minWidth: 275,
+        },
+        bullet: {
+            display: 'inline-block',
+            margin: '0 2px',
+            transform: 'scale(0.8)',
+        },
+        title: {
+            fontSize: 14,
+        },
+        pos: {
+            marginBottom: 12,
+        },
+    });
+
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
 
     // let mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
@@ -16,7 +42,27 @@ function MapSimple() {
             />
             <Marker position={[51.505, -0.09]}>
                 <Popup>
-                    
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                Word of the Day
+                            </Typography>
+                            <Typography variant="h5" component="h2">
+                                be{bull}nev{bull}o{bull}lent
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                adjective
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Learn More</Button>
+                        </CardActions>
+                    </Card>
                 </Popup>
             </Marker>
         </MapContainer>
