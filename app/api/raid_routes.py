0b2400c209c3d531ca80279bb3d1f3806afd7fae
philling83 <philipling83@gmail.com>
2021-09-09 13:48:28 -0400

    Edit raid route to only fetch active raids

--- app/api/raid_routes.py
+++ app/api/raid_routes.py
@@ -6,5 +6,5 @@ raid_routes = Blueprint('raids', __name__)
 
 @raid_routes.route('/')
 def raids():
-    raids = Raid.query.all()
+    raids = Raid.query.filter_by(is_active=True)
     return {"raids": [raid.to_dict() for raid in raids]}
