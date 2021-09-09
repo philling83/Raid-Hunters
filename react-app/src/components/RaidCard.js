7e6ca54763b35fa9451b81ac0d47b3b0f71b8499
philling83 <philipling83@gmail.com>
2021-09-09 11:15:13 -0400

    Add flip effect to RaidCard

--- react-app/src/components/RaidCard.js
+++ react-app/src/components/RaidCard.js
@@ -5,6 +5,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import "./RaidCard.css";
 import { fetchRaids } from "../actions/raids_actions";
 
 const RaidCard = ({ fetch, raids }) => {
@@ -14,15 +15,26 @@ const RaidCard = ({ fetch, raids }) => {
         console.log(raids);
     }, [raids.length]);
 
-    return raids.map((raid, i) => 
-        <Card key={i}>
-            <CardContent>
-                <img src={raid.url_img} alt={raid.pokemon} />
-                <Typography>
-                    {raid.pokemon}
-                </Typography>
-            </CardContent>
-        </Card>
+    return raids.map((raid, i) =>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <Card key={i} variant="outlined" className="raid_card flip-card-front">
+                    <CardContent>
+                        <img src={raid.url_img} alt={raid.pokemon} />
+                        <Typography>
+                            {raid.pokemon}
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card key={i} variant="outlined" className="raid_card flip-card-back">
+                    <CardContent>
+                        <Typography>
+                            {raid.pokemon}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div> 
+        </div>
     )
 };
 
