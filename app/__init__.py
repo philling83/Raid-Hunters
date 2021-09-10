428131de3475fc17f503461c5a001ececab776b4
philling83 <philipling83@gmail.com>
2021-09-10 10:26:01 -0400

    https redirect

--- app/__init__.py
+++ app/__init__.py
@@ -47,15 +47,12 @@ CORS(app)
 
 
 @app.before_request
-def before_request():
-    if app.env == "development":
-        return
-    if request.is_secure:
-        return
-
-    url = request.url.replace("http://", "https://", 1)
-    code = 301
-    return redirect(url, code=code)
+def https_redirect():
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
 
 
 @app.after_request
